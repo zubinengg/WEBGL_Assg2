e0c4351150785cf45f132a22c7d2b8b612e082c4
home <home@home-Inspiron-580>
2015-07-26 15:57:22 +0530

    Assg2_Correction

--- public_html/index.js
+++ public_html/index.js
@@ -2,12 +2,8 @@
 var canvas;
 var gl;
 
-var maxNumTriangles = 200;  
-var maxNumVertices  = 3 * maxNumTriangles;
+var maxNumVertices  = 10000;
 var index = 0;
-var first = true;
-
-var t1, t2, t3, t4;
 
 var cIndex = 0;
 
@@ -21,90 +17,95 @@ var colors = [
     vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
 ];
 
+var t;
+var draw = false;
+var numLineSegments = 0;
+var numIndices = [];
+numIndices[0] = 0;
+var start = [0];
 
 window.onload = function init() {
     canvas = document.getElementById( "gl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    // Color menu
+    var colorMenu = document.getElementById("mymenu");
     
+    colorMenu.addEventListener("click", function() {
+       cIndex = colorMenu.selectedIndex;
+    });
+    
+    
+
+    
+    canvas.addEventListener("mousedown", function(event){
+	draw = true;
+	console.log("mousedown "+canvas.width+" x "+canvas.height);
+    } );
+    canvas.addEventListener("mouseup", function(event){
+	draw = false;
+	numLineSegments++;
+	numIndices[numLineSegments] = 0;
+	start[numLineSegments] = index;
+	render();
+    });
+    canvas.addEventListener("mousemove", function(event){
+	if (draw) {
+	    var x = -1+2*event.clientX/canvas.width 
+	    var y = -1+2*(canvas.height-event.clientY)/canvas.height;
+	    t = vec2(x,y);
+	    //console.log("x="+x+" y="+y);
+	    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId);
+	    gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
+
+	    t = vec4(colors[cIndex]);
+
+	    gl.bindBuffer( gl.ARRAY_BUFFER, cBufferId);
+	    gl.bufferSubData(gl.ARRAY_BUFFER, 16*index, flatten(t));
+
+	    numIndices[numLineSegments]++;
+	    index++;
+	    render();
+	}
+    });
+
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
     gl.clear( gl.COLOR_BUFFER_BIT );
-
-
     //
     //  Load shaders and initialize attribute buffers
     //
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
-    
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, 8*maxNumVertices, gl.STATIC_DRAW);
     
     var vPosition = gl.getAttribLocation( program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
     
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    var cBufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
     gl.bufferData(gl.ARRAY_BUFFER, 16*maxNumVertices, gl.STATIC_DRAW );
     
     var vColor = gl.getAttribLocation( program, "vColor");
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
-    
-    var m = document.getElementById("mymenu");
-    
-    m.addEventListener("click", function() {
-       cIndex = m.selectedIndex;
-        });
-
-    
-    canvas.addEventListener("mousedown", function(event){
-        gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-        if(first) {
-          first = false;
-          gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-          t1 = vec2(2*event.clientX/canvas.width-1, 
-            2*(canvas.height-event.clientY)/canvas.height-1);
-        }
-        else {
-          first = true;
-          t2 = vec2(2*event.clientX/canvas.width-1, 
-            2*(canvas.height-event.clientY)/canvas.height-1);
-          t3 = vec2(t1[0], t2[1]);
-          t4 = vec2(t2[0], t1[1]);
-
-          gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t1));
-          gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+1), flatten(t3));
-          gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+2), flatten(t2));
-          gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+3), flatten(t4));
-          gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
-          index += 4;
-          
-          t = vec4(colors[cIndex]);
-
-          gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-4), flatten(t));
-          gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-3), flatten(t));
-          gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-2), flatten(t));
-          gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-1), flatten(t));
-        }
-    } );
-
-    render();
-};
+    //render();
+}
 
 
 function render() {
     
     gl.clear( gl.COLOR_BUFFER_BIT );
 
-    for(var i = 0; i<index; i+=4)
-        gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
-
-    window.requestAnimFrame(render);
+    for(var i = 0; i<=numLineSegments; i++) {
+        gl.drawArrays( gl.LINE_STRIP, start[i], numIndices[i] );
+    }
+    //window.requestAnimFrame(render);
 
 }
